810df8d2f5b9f000cde69156320c1fce762062bf
Enis <enis_lalmi@hotmail.com>
2021-07-14 11:36:06 +0200

    initial commit with needed libraries

---
+++ TwitterMapperStarter/src/twitter/LiveTwitterSource.java
@@ -0,0 +1,58 @@
+package twitter;
+
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ * Encapsulates the connection to Twitter
+ *
+ * Terms to include in the returned tweets can be set with setFilterTerms
+ *
+ * Implements Observable - each received tweet is signalled to all observers
+ */
+public class LiveTwitterSource extends TwitterSource {
+    private TwitterStream twitterStream;
+    private StatusListener listener;
+
+    public LiveTwitterSource() {
+        initializeTwitterStream();
+    }
+
+    protected void sync() {
+        FilterQuery filter = new FilterQuery();
+        // https://stackoverflow.com/questions/21383345/using-multiple-threads-to-get-data-from-twitter-using-twitter4j
+        String[] queriesArray = terms.toArray(new String[0]);
+        filter.track(queriesArray);
+
+        System.out.println("Syncing live Twitter stream with " + terms);
+
+        twitterStream.filter(filter);
+    }
+
+    private void initializeListener() {
+        listener = new StatusAdapter() {
+            @Override
+            public void onStatus(Status status) {
+                // This method is called each time a tweet is delivered by the twitter API
+                if (status.getPlace() != null) {
+                    handleTweet(status);
+                }
+           }
+        };
+    }
+
+    // Create ConfigurationBuilder and pass in necessary credentials to authorize properly, then create TwitterStream.
+    // Create ConfigurationBuilder and pass in necessary credentials to authorize properly, then create TwitterStream.
+    private void initializeTwitterStream() {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setOAuthConsumerKey("JeE2S8KXWwVp0yEYrKhsv7lUE")
+                .setOAuthConsumerSecret("BhatTGhgfDcJSd4lJRV1Cw6CE1I9MFT8LtzwKkRSNXW042qCmk")
+                .setOAuthAccessToken("1282629240396156929-XA4r8GY6eTSsX1UdfBVklpmwNkfLUr")
+                .setOAuthAccessTokenSecret("IBFbR3d5eeOqXJchVV2o09fuNHLQisNYo2n3qC4PFEccQ");
+
+        // Pass the ConfigurationBuilder in when constructing TwitterStreamFactory.
+        twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+        initializeListener();
+        twitterStream.addListener(listener);
+    }
+}
